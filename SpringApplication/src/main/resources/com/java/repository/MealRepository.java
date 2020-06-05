35045cc9003a43a152ce7b9b1feb4bbb9aa7b7b8
JImmyAngelProject <63358779+JImmyAngelProject@users.noreply.github.com>
2020-06-05 18:41:46 +0300

    Add files via upload

---
+++ SpringApplication/src/main/resources/com/java/repository/MealRepository.java
@@ -0,0 +1,10 @@
+package com.example.repository;
+
+public class MealRepository extends JpaRepository{
+
+public void setMeal();
+public void setMeals();
+public Meal getMeal();
+public List<Meal>getAllMeals();
+
+}
